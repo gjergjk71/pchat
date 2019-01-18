58dc8ab1fe2589ca6e3ac542f20777cda5adbc9d
gjergjk71 <gjergjk71@gmail.com>
2019-01-18 22:25:34 +0100

    Included messages.js in app

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var bearerToken = require("express-bearer-token");
 var models = require("./models");
 var auth_routes = require("./routes/auth");
 var users_route = require("./routes/users");
+var messages_route = require("./routes/messages");
 
 models.sequelize.sync();
 
@@ -17,5 +18,6 @@ app.use(bearerToken());
 
 app.use("/auth/",auth_routes);
 app.use("/api/users/",users_route)
+app.use("/api/messages/",messages_route)
 var port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on ${port}`))
