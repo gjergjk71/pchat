b621fc79a7510487bb751924fcb5153be09c2f02
gjergjk71 <gjergjk71@gmail.com>
2019-01-18 21:33:40 +0100

    Added app.listen(..)

--- server/app.js
+++ server/app.js
@@ -19,10 +19,3 @@ app.use("/auth/",auth_routes);
 app.use("/api/users/",users_route)
 var port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on ${port}`))
-
-
-// app.get("/protected",(req,res) => {
-// 	middleware.verifyJWT(req,res,(req,res) => {
-// 		res.send(req.decoded);
-// 	})
-// })
