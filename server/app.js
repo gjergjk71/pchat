69c180365796298e6030e1f4a248ed220297f06d
Gjergj Kadriu <gjergjk71@gmail.com>
2019-01-21 16:32:42 +0100

    Added conversations/participants routes

--- server/app.js
+++ server/app.js
@@ -7,8 +7,10 @@ var models = require("./models");
 var auth_routes = require("./routes/auth");
 var users_route = require("./routes/users");
 var messages_route = require("./routes/messages");
+var conversations_route = require("./routes/conversations");
+var participants_route = require("./routes/participants");
 
-models.sequelize.sync();
+models.sequelize.sync({force:true});
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,6 +20,13 @@ app.use(bearerToken());
 
 app.use("/auth/",auth_routes);
 app.use("/api/users/",users_route)
+app.use("/api/conversations/",messages_route)
+app.use("/api/participants/",participants_route)
 app.use("/api/messages/",messages_route)
+
+app.use((req,res,next) => {
+	res.status(404);
+	res.send("404 Page not found!");
+})
 var port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on ${port}`))
